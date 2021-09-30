94bc7db02dfe653bcad132d2e01036b98cf71ceb
BrendonHawkins <artbitcodedata@gmail.com>
2021-09-30 00:55:46 -0400

    Delete works

--- api/server.js
+++ api/server.js
@@ -34,19 +34,15 @@ server.get('/api/users/:id', (req, res) => {
   });
 
   server.post("/api/users", (req, res) => {
-    const { name, bio } = req.body;
-    if (!name || !bio) {
+    const user = req.body;
+    if (!user.name || !user.bio) {
         res.status(400).json({
         message: "Please provide name and bio for the post",
         });
     } else {
-        Users.insert(req.body)
+        Users.insert(user)
         .then((id) => {
-            res.status(201).json({
-                id: id,
-                name: name,
-                bio: bio
-            });
+            res.status(201).json(id);
         })
         .catch((error) => {
             console.log(error);
@@ -68,16 +64,18 @@ server.get('/api/users/:id', (req, res) => {
     })
   });
 
-  server.delete('/api/users', (req, res) => {
-    Users.find(req.query)
-    .then(user =>{
-        res.status(200).json(user)
-    }) 
-    .catch(err =>{
-        res.status(500).json({
-            message: err.message
-        })
-    })
+  server.delete('/api/users/:id', async (req, res) => {
+    const possUser = await Users.findById(req.params.id)
+    if(!possUser) {
+        res.status(404).json(
+            { message: "The user with the specified ID does not exist" }
+        )
+    } else {
+        const removedUser = await Users.remove(possUser.id)
+        res.status(200).json(removedUser)
+
+        
+    }
   });
 
 server.get('*', (req, res) => {
