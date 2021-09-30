36ab9a8d7a06067a0af94fb97a7ae14588b11523
BrendonHawkins <artbitcodedata@gmail.com>
2021-09-30 01:12:29 -0400

    Success

--- api/server.js
+++ api/server.js
@@ -52,19 +52,33 @@ server.get('/api/users/:id', (req, res) => {
     })
   }});
 
-  server.put('/api/users', (req, res) => {
-    Users.find(req.query)
-    .then(user =>{
-        res.status(200).json(user)
-    }) 
-    .catch(err =>{
-        res.status(500).json({
-            message: err.message
-        })
-    })
+  server.put('/api/users/:id', async (req, res) => {
+    try{
+        const possUser = await Users.findById(req.params.id)
+        if(!possUser) {
+            res.status(404).json(
+                { message: "The user with the specified ID does not exist" }
+            )
+        } else {
+            if(!req.body.name || !req.body.bio) {
+                res.status(400).json(
+                    { message: "Please provide name and bio for the user" }
+                )
+            } else{
+               const updatedUser = await Users.update(req.params.id, req.body)
+               res.status(200).json(updatedUser)
+            }
+        } 
+       } catch(err){
+        console.log(err);
+                res.status(500).json({
+                message: 'Error retrieving the posts',
+            });
+       }
   });
 
   server.delete('/api/users/:id', async (req, res) => {
+   try{
     const possUser = await Users.findById(req.params.id)
     if(!possUser) {
         res.status(404).json(
@@ -73,9 +87,13 @@ server.get('/api/users/:id', (req, res) => {
     } else {
         const removedUser = await Users.remove(possUser.id)
         res.status(200).json(removedUser)
-
-        
     }
+   } catch(err){
+    console.log(err);
+            res.status(500).json({
+            message: 'Error retrieving the posts',
+        });
+   }
   });
 
 server.get('*', (req, res) => {
