2198322bafbae395701f55ce837f2284a0aa1fc2
BrendonHawkins <artbitcodedata@gmail.com>
2021-09-30 00:21:52 -0400

    get and post working

--- api/server.js
+++ api/server.js
@@ -9,8 +9,76 @@ server.get('/api/users', (req, res) => {
   Users.find(req.query)
   .then(user =>{
       res.status(200).json(user)
+  }) 
+  .catch(err =>{
+      res.status(500).json({
+          message: err.message,
+      })
   })
 });
+
+server.get('/api/users/:id', (req, res) => {
+    Users.findById(req.params.id)
+    .then(user =>{
+        if(user){
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        }
+    }) 
+    .catch(err =>{
+        res.status(500).json({
+            message: err.message
+        })
+    })
+  });
+
+  server.post("/api/users", (req, res) => {
+    const { name, bio } = req.body;
+    if (!name || !bio) {
+        res.status(400).json({
+        message: "Please provide name and bio for the post",
+        });
+    } else {
+        Users.insert(req.body)
+        .then((id) => {
+            res.status(201).json({
+                id: id,
+                name: name,
+                bio: bio
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).json({
+            message: 'Error retrieving the posts',
+        });
+    })
+  }});
+
+  server.put('/api/users', (req, res) => {
+    Users.find(req.query)
+    .then(user =>{
+        res.status(200).json(user)
+    }) 
+    .catch(err =>{
+        res.status(500).json({
+            message: err.message
+        })
+    })
+  });
+
+  server.delete('/api/users', (req, res) => {
+    Users.find(req.query)
+    .then(user =>{
+        res.status(200).json(user)
+    }) 
+    .catch(err =>{
+        res.status(500).json({
+            message: err.message
+        })
+    })
+  });
 
 server.get('*', (req, res) => {
     res.status(404).json({
